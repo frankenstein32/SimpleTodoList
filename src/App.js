eba0444e969ed4e11cd835c86972917acaf06f6b
frankenstein32 <saifirashid32@gmail.com>
2020-09-02 14:39:45 +0530

    Functionality added

--- src/App.js
+++ src/App.js
@@ -1,26 +1,63 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {Component} from 'react';
 import './App.css';
+import Todos from './components/Todos'
+import Header from './components/layout/Header'
+class App extends Component{
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  state = {
+    todos: [
+
+      {
+        id: 1,
+        title: "Go to Gym",
+        completed: false
+      },
+      {
+        id: 2,
+        title: "Have a good diet breakfast",
+        completed: false
+      },
+      {
+        id: 3,
+        title: "Prepare for Office",
+        completed: false
+      },
+      {
+        id: 4,
+        title: "clean Car",
+        completed: false
+      },
+      {
+        id: 5,
+        title: "Check all car-documents",
+        completed: false
+      },
+
+    ]
+  }
+
+  markComplete = (id) => {
+    this.setState({
+      todos: this.state.todos.map(todo => {
+        if(todo.id == id){
+          todo.completed = !todo.completed;
+        }
+        return todo;
+      })
+    })
+  }
+
+  delTodo = (id) => {
+    this.setState({todos: [...this.state.todos.filter(todo => todo.id != id)]});
+  }
+    render(){
+      return (
+        <div className="App">
+           <Header/>
+          <Todos todos={this.state.todos} markComplete={this.markComplete} delTodo={this.delTodo}/>
+        </div>
+      );
+    }
 }
-
 export default App;
+ 
